31ba5875bc275ef108afdb68fe57b85a04f67d26
paradigmn <r.prokscha@oth-aw.de>
2016-05-13 20:02:49 +0000

    minor corrections

--- error.c
+++ error.c
@@ -6,7 +6,7 @@ void error(int code)
     switch(code)
     {
         case 1: printf("WiringPi is not installed or missing\n"); break;
-        case 2: printf("Invalid Input, type 'wifiplug [Plug(0-31)] [Remote(0-31)] [State(On/Off)]'\n"); break;
+        case 2: printf("Invalid Input, type 'wifiplug [Remote(0-31)] [Plug(0-31)] [State(On/Off)]'\n"); break;
         case 3: printf("Calculation error\n"); break;
         default: printf("An unknown error occurred\n"); break;
     }
